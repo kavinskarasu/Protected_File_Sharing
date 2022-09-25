953de0a716ead189b0300d16393b5dd4f50c70f4
kavinskarasu <kavinskarasu@gmail.com>
2022-09-25 13:22:39 +0530

    updated

---
+++ File.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+
+const fileSchema=new mongoose.Schema({
+    path:{
+        type:String,
+        required:true
+    },
+    originalName:{
+        type:String,
+        required:true
+    },
+    password:String,
+    downloadCount:{
+        type:Number,
+        required:true,
+        default:0
+    }
+})
+
+const file=mongoose.model("file",fileSchema);
+module.exports=file;
--- server.js
+++ server.js
@@ -1,17 +1,67 @@
 const express=require('express');
 const ejs=require('ejs');
+const mongoose=require('mongoose');
 const multer=require('multer')
 const app=express();
 const upload=multer({dest:"uploads"})
+const bcrypt=require('bcrypt')
+const fileCreate=require('./File')
+const dotenv=require('dotenv');
+const { removeListener } = require('./File');
+dotenv.config();
+app.use(express.json());
+console.log(process.env.DB);
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!")).catch((err)=>{
+console.log(err)
+});
 app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{
+  console.log(await fileCreate.find())
     res.render("index");
+   
 })
-app.post("/upload",upload.single("file"),(req,res)=>{
-    res.send("hi")
+app.post("/upload",upload.single("file"),async(req,res)=>{
+    const fileData={
+        path:req.file.path,
+        originalName:req.file.originalname
+
+    }
+    if(req.body.password!=null&&req.body.password!=""){
+        fileData.password= await bcrypt.hash(req.body.password,10)
+    }
+    const files=await fileCreate.create(fileData);
+  res.render("index",{fileLink:`${req.headers.origin}/file/${files.id}`})
+     
+    
 })
 
-app.listen(4000,()=>{
-console.log("server running on port 4000");
+app.get("/file/:id",async(req,res)=>{
+  const passfile=await fileCreate.findById(req.params.id);
+  if(passfile.password!=null){
+      if(req.password==null){
+        res.render("password");
+        return
+      }
+if(!await bcrypt.compare(req.body.password,passfile.password)){
+  res.render("password",{error:true});
+  return
+}
+    
+  }
+  file.downloadCount++;
+
+  await file.save()
+  res.download(file.path,file.originalName);
+})
+
+app.listen(process.env.PORT,()=>{
+console.log(`server is running on port ${process.env.PORT}`);
 })
