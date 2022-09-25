9c9daa674406029c5be1e4e74d9d0cba850f53be
kavinskarasu <kavinskarasu@gmail.com>
2022-09-25 09:57:46 +0530

    updated

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express=require('express');
 const ejs=require('ejs');
 const multer=require('multer')
 const app=express();
-
+const upload=multer({dest:"uploads"})
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
     res.render("index");
+})
+app.post("/upload",upload.single("file"),(req,res)=>{
+    res.send("hi")
 })
 
 app.listen(4000,()=>{
